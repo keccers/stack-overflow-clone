6bdff42d5ee81277bbc75c0720bef5b62a2bbfdd
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 16:15:18 -0500

    added create and destroy session methods in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,19 @@
 class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url
+    else
+      @errors = user.errors.full_messages
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url
+  end
   
 end
