4e1d50e165b43a54df76e1ed373cfe900fc5b03b
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 11:23:45 -0500

    added create comments migration

---
+++ db/migrate/20130807162129_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.text :text
+      t.integer :commentable_id
+      t.string :commentable_type
+      t.integer :commenter_id
+    end
+  end
+end
