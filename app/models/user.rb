ffe6f2cc07acc76c60658972e3401d28c8dcf4cb
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 13:25:42 -0500

    created user model validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
-  
+  attr_accessible :name, :email, :password, :password_confirmation, :about
+  validates_presence_of :name, :email, :password, :password_confirmation, :about
+  validates :email, format: {with: /.+@.+\..+/}, uniqueness: true
+
+  has_many :questions, foreign_key: 'asker_id'
+  has_many :answers, foreign_key: 'responder_id'
+  has_many :comments, foreign_key: 'commenter_id'
+  has_many :votes
+
 end
