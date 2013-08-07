3f6717e1175f8faea6f8d3850aec11cdb0af2b94
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 16:25:20 -0500

    added new user method, new session method with authentication, and destroy session method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  
+
   helper_method :current_user
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 class UsersController < ApplicationController
   
+  def create
+    @user = User.create(params[:user])
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      @errors = @user.errors.full_messages
+      render 'new'
+    end
+  end
+  
 end
