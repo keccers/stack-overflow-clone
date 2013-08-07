f43671c9789541012cafc7eced342b1405db6d6c
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 13:57:32 -0500

    created comment model validations and associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
-  
+  validates_presence_of :text, :commentable_id, :commentable_type, :commenter_id
+
+  belongs_to :commentable, polymorphic: true
+  belongs_to :commenter, class_name: "User"
 end
