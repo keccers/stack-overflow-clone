581d6030cd1a184ba71bdad0a36352d0a7e07571
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 13:35:10 -0500

    created question model validations and associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,12 @@
 class Question < ActiveRecord::Base
-  
+  attr_accessible :title, :text
+  validates_presence_of :title, :text, :asker_id
+
+  belongs_to :asker, class_name: "User"
+  has_many :answers
+  has_one :best_answer, class_name: "Answer"
+  has_many :comments, as: :commentable
+  has_many :votes, as: :votable
+  has_many :questions_tags
+  has_many :tags, through: :questions_tags
 end
