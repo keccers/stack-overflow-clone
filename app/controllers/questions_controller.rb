4fe76fd2b3302ccc0da2bce17321fc47ed734a04
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 18:37:14 -0500

    rebased questions controller to bring in user authentication for questions pages

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,12 +2,16 @@ class QuestionsController < ApplicationController
   before_action :user_signed_in?, only: :create
   before_action :correct_user, only: :destroy
 
+  def index
 
+  end
 
+  def show
 
-
+  end
 
   private
+
     def correct_user
       question = Question.find(params[:id])
       redirect_to root_url unless current_user && question.asker_id == current_user.id
