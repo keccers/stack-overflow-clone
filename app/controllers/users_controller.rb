c0b48e353be108ff049d4901dfafd5c82e45345e
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 16:37:12 -0500

    added user index, edit, update, and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
   
   def create
     @user = User.create(params[:user])
@@ -10,5 +14,33 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
+  def new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to @user
+    else
+      @errors = @user.errors.full_messages
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_url
+  end
+
 end
