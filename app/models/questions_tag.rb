768d978815166d60e3b2bd6a2c4dbfc64d0bf048
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 20:06:19 -0500

    added validation for uniqueness of question tag pair

--- app/models/questions_tag.rb
+++ app/models/questions_tag.rb
@@ -1,5 +1,6 @@
 class QuestionsTag < ActiveRecord::Base
   validates_presence_of :question_id, :tag_id
+  validates_uniqueness_of :question_id, :scope => :tag_id
 
   belongs_to :question
   belongs_to :tag
