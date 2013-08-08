26c706f9b69127587dc42d8132b5e19c934f6e14
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-08 15:48:48 -0500

    updated validations for votes to ensure uniqueness

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,7 @@
 class Vote < ActiveRecord::Base
+  attr_accessible :value, :votable_type, :votable_id
   validates_presence_of :user_id, :votable_id, :votable_type, :value
+  validates_uniqueness_of :user_id, :scope => [:votable_id, :votable_type]
   
   belongs_to :user
   belongs_to :votable, polymorphic: true
