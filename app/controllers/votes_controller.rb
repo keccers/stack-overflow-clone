f65371af01a0b84b77963d5292b7942e30ced456
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-08 15:46:45 -0500

    created voting and redirect system for voting for questions and answers

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,3 +1,20 @@
 class VotesController < ApplicationController
-  
+  before_filter :find_votable
+
+  def new
+    @vote = Vote.new
+  end
+
+  def create
+    @vote = current_user.votes.new(params[:vote])
+    @errors = @vote.errors.full_messages unless @vote.save
+    ( @vote.votable_type == "Question" ) ? ( redirect_to @vote.votable ) : ( redirect_to question_path(@vote.votable.question) )
+  end
+
+  private
+
+  def find_votable
+    klass, id = request.path.split('/')[1, 2]
+    @votable = klass.singularize.classify.constantize.find(id)
+  end
 end
