fe63e21474951fcd6e25e0c0af98a057a172c219
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-08 14:33:03 -0500

    added aliases for sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,9 @@ DbcOverflow::Application.routes.draw do
 
   resources :users
 
-  get     '/login' => 'sessions#new'
-  post    '/login' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
+  get     '/login' => 'sessions#new', :as => :login
+  post    '/login' => 'sessions#create', :as => :login
+  delete '/logout' => 'sessions#destroy', :as => :logout
 
   root :to => "questions#index"
 end
