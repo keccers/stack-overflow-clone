702138302205d9e2d8cbdfead3713a16fa8d0910
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 20:43:08 -0500

    setup answer views and creation for questions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,11 @@
 class AnswersController < ApplicationController
-  
+  def create
+    @answer = current_user.answers.create(params[:answer])
+    if @answer.valid?
+      redirect_to @answer.question
+    else
+      @errors = @answer.errors
+      render @answer.question
+    end
+  end
 end
