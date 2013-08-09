346de3dc0768cb949d12a8542d555ddc8ad0ea9f
kevinbuch <kevaustinbuch@gmail.com>
2013-08-08 20:15:41 -0500

    changed test to reflect belongs to association

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -6,7 +6,7 @@ describe Question do
   it { should validate_presence_of(:asker_id) }
   it { should belong_to(:asker) }
   it { should have_many(:answers) }
-  it { should have_one(:best_answer) }
+  it { should belong_to(:best_answer) }
   it { should have_many(:comments) }
   it { should have_many(:votes) }
   it { should have_many(:questions_tags) }
